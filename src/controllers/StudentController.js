fc6734005fdd4006b27e9a937ceac76c58b43391
Abdul Hamid <120128928+hamidbd2310@users.noreply.github.com>
2024-01-28 19:16:35 +0600

    expressJs

---
+++ src/controllers/StudentController.js
@@ -0,0 +1,14 @@
+exports.CreateStudent = (req, res) => {
+    
+}
+
+exports.DeleteStudent = (req, res) => {
+    
+}
+exports.UpdateStudent = (req, res) => {
+    
+}
+
+exports.ReadStudent = (req, res) => {
+    
+}
---
+++ src/routes/api.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const StudentController = require('../controllers/StudentController');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    
+})
+
+router.post('/create',StudentController.CreateStudent, (req, res) => {
+    
+})
+
+router.put('/', (req, res) => {
+    
+})
+
+router.delete('/', (req, res) => {
+    
+})
